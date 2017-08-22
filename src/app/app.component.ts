5ebcfff2112408d9fd9d78da9a74f28ca12731e9
GUSTAVO PEREIRA DE SOUZA <01621094154@sefaz.to.gov.br>
2017-08-22 17:37:15 -0300

    Att: Esta 90% funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,30 +9,77 @@ import '../assets/css/style.css';
 })
 export class AppComponent implements OnInit {
     title: string;
-    valor: Int32Array;
-    resultado = Float64Array;
-    A1 = Float32Array ;
+    tela: string;
+    valor: number;
+    sinal : string;
+    total : number;
+    aux: string;
+    resultado: number;
 
     constructor() {
         this.title = "Calculadora do Gustavo";
+        this.tela = "";
+        this.total = 0;
+        this.aux="";
     }
 
     ngOnInit(): void {
     }
     
     calcNum(num){
-        var aux = this.valor;
-        this.valor = aux+num;
+        this.tela = this.tela + num;
+        if (this.sinal=="+" || this.sinal=="-"){
+            this.aux = this.aux+num;
+        }
     }
-    
     limpar(){
-        this.valor = null
+        this.tela = "";
+        this.valor= 0;
+        this.sinal="";
+        this.aux="";
+        this.resultado = 0;
     }
     calcSomar(){
-        var aux = this.valor;
-        this.valor = aux;
+        if (this.resultado > 0){
+            this.valor = this.resultado
+            this.sinal = "+";
+            this.tela = this.valor+"+";
+            this.aux ="";
+        }
+        else{
+            this.valor = parseInt(this.tela);
+            this.sinal = "+";
+            this.tela = this.valor+"+";
+            this.aux ="";
+        }
+    }
+    calcSubtrair(){
+        if (this.resultado > 0){
+            this.valor = this.resultado
+            this.sinal = "-";
+            this.tela = this.valor+"-";
+            this.aux ="";
+        }
+        else{
+            this.valor = parseInt(this.tela);
+            this.sinal = "-";
+            this.tela = this.valor+"-";
+            this.aux ="";
+        }
     }
     Resultado(){
-        
+        if (this.sinal == "+"){
+            this.resultado = (this.valor) + (parseInt(this.aux));
+            this.valor = 0;
+            this.sinal="";
+            this.tela = this.resultado+"";          
+        }
+        else if(this.sinal == "-"){
+            this.resultado = (this.valor) - (parseInt(this.aux));
+            this.valor = 0;
+            this.sinal="";  
+            this.tela = this.resultado+"";
+        }
+        this.sinal="";     
     }
 }
